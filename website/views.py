5b37150236e7dcab6d321e767a0fc600cd721aa3
Agent 7 <Agent7@FLSystems.Local>
2019-07-26 17:00:45 +0530

    files renamed with _3

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, JsonResponse
 
 
 def homepage(request):
-    return render(request, 'home_without_compress.html')
+    return render(request, 'home.html')
 
 
 def enquiry_form(request):
